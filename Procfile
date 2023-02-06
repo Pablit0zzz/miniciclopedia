31d0d9318245b83b256e6e2a6d188016a5619ef6
Pablit0zzz <pablopris2000zzz@gmail.com>
2023-02-06 20:44:51 +0300

    make it better

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn HelloShreck.wsgi --log-file -
--- nedorostock/pypypy.py
+++
@@ -1,4 +0,0 @@
-with open("C:/Users/pablo/OneDrive/Рабочий стол/Python/nedorostock/nedorostock/Terms of use.txt", encoding="utf-8") as file:
-    text = file.read()
-    print(text)
-    input()
